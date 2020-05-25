ce28134fb0b1deee7478c13ef6506ae5faebbce8
momolela <1083910359@qq.com>
2020-05-25 23:34:59 +0800

    20200525

--- base/src/main/java/com/momolela/thread/Thread06ThreadCommunication.java
+++ base/src/main/java/com/momolela/thread/Thread06ThreadCommunication.java
@@ -7,6 +7,33 @@ class Res {
     String name;
     String sex;
     boolean flag = false; // 当为false的时候Input执行，当为true的时候Output执行，保证输入和输出均匀
+
+    synchronized void in(String name, String sex) {
+        if (this.flag) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.name = name;
+        this.sex = sex;
+        this.flag = true;
+        this.notify();
+    }
+
+    synchronized void out() {
+        if (!this.flag) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(this.name + " === " + this.sex);
+        this.flag = false;
+        this.notify();
+    }
 }
 
 /**
@@ -23,25 +50,33 @@ class Input implements Runnable {
     public void run() {
         int x = 0;
         while (true) {
-            synchronized (s) { // 相同的s锁，解决安全问题
-                if (s.flag) {
-                    try {
-                        s.wait(); // 当s.flag为true，当前Input线程wait
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (x == 0) {
-                    s.name = "szj";
-                    s.sex = "male";
-                } else {
-                    s.name = "hfy";
-                    s.sex = "female";
-                }
-                x = (x + 1) % 2;
-                s.flag = true; // 如果执行完了，就把s.flag设置为true，为了就是下次wait
-                s.notify(); // 将s对象锁的其他线程唤醒
+//            synchronized (s) { // 相同的s锁，解决安全问题
+//                if (s.flag) {
+//                    try {
+//                        s.wait(); // 当s.flag为true，当前Input线程wait
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//                if (x == 0) {
+//                    s.name = "szj";
+//                    s.sex = "male";
+//                } else {
+//                    s.name = "hfy";
+//                    s.sex = "female";
+//                }
+//                x = (x + 1) % 2;
+//                s.flag = true; // 如果执行完了，就把s.flag设置为true，为了就是下次wait
+//                s.notify(); // 将s对象锁的其他线程唤醒
+//            }
+
+            // 优化代码
+            if (x == 0) {
+                s.in("szj", "male");
+            } else {
+                s.in("hfy", "female");
             }
+            x = (x + 1) % 2;
         }
     }
 }
@@ -59,18 +94,21 @@ class Output implements Runnable {
     @Override
     public void run() {
         while (true) {
-            synchronized (s) { // 相同的s锁，解决安全问题
-                if (!s.flag) {
-                    try {
-                        s.wait(); // 当s.flag为false，当前Output线程wait
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                System.out.println(s.name + " === " + s.sex);
-                s.flag = false; // 如果执行完了，就把s.flag设置为false，为了就是下次wait
-                s.notify(); // 将s对象锁的其他线程唤醒
-            }
+//            synchronized (s) { // 相同的s锁，解决安全问题
+//                if (!s.flag) {
+//                    try {
+//                        s.wait(); // 当s.flag为false，当前Output线程wait
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//                System.out.println(s.name + " === " + s.sex);
+//                s.flag = false; // 如果执行完了，就把s.flag设置为false，为了就是下次wait
+//                s.notify(); // 将s对象锁的其他线程唤醒
+//            }
+
+            // 优化代码
+            s.out();
         }
     }
 }
@@ -80,7 +118,7 @@ class Output implements Runnable {
  * notify()
  * notifyAll()
  * 都使用在同步中，因为要对锁的线程操作，只有同步才有锁
- *
+ * <p>
  * 为什么这些操作方法都定义在Object类中呢？
  * 这些方法在操作同步中线程时，都必须要标识他们所操作线程的锁，
  * 只有同一个锁上被等待的线程能被同一个锁上的notify()唤醒，不可以对不同锁中的线程进行唤醒，
