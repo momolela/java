da0408883150eee5df85819778a7f33a28aed201
sunzj <1083910359@qq.com>
2020-07-22 09:06:24 +0800

    20200722

--- base/src/main/java/com/momolela/io/randomaccessfile/IORandomAccessFile01Base.java
+++ base/src/main/java/com/momolela/io/randomaccessfile/IORandomAccessFile01Base.java
@@ -5,9 +5,9 @@ import java.io.RandomAccessFile;
 
 
 /**
- * RandomAccessFile 不属于 IO 体系，但是在 IO 包中成员，因为他包含读和写的功能，内部就是用字节流实现，只有流才能操作数据
+ * RandomAccessFile 不属于 IO 体系，但是在 IO 包中成员，因为他直接就包含读和写的功能，内部就是用字节流实现，因为只有流才能操作数据
  * 内部封装了一个数组，通过指针操作数组元素，可以通过 .getFilePointer() 方法可以获取指针位置，也可以通过 .seek() 改变指针位置
- * 通过构造函数，这个类只能操作文件，而且 mode 可以有 r rw ... 等模式
+ * 通过他的构造函数，得知：这个类只能操作文件，而且 mode 可以有 r rw ... 等模式
  * r 只读模式，不能创建文件，会读文件，文件不存在会抛出异常
  * rw 读写模式，要操作的文件不存在会创建，存在不会覆盖
  * 该类可用于多线程读取文件，下载文件，想起迅雷
@@ -26,7 +26,7 @@ public class IORandomAccessFile01Base {
         RandomAccessFile randomAccessFile = new RandomAccessFile("test-random.txt", "rw");
         randomAccessFile.write("张三".getBytes()); // 默认以 utf-8 写入
 //        randomAccessFile.write(258); // 这个写法会丢失精度，因为 write 只会写 8 个字节，258 是 1 00000010，前面的 1 会丢失
-        randomAccessFile.writeInt(258);
+        randomAccessFile.writeInt(258); // 这个写法不会丢失精度
         randomAccessFile.close();
     }
 
@@ -35,7 +35,7 @@ public class IORandomAccessFile01Base {
         byte[] buff = new byte[6]; // utf-8 的编码，一个中文 3 个字节
         randomAccessFile.read(buff);
         String name = new String(buff);
-//        String name = new String(randomAccessFile.readLine().getBytes("ISO-8859-1"), "utf-8"); // readLine() 需要转码
+//        String name = new String(randomAccessFile.readLine().getBytes("ISO-8859-1"), "utf-8"); // readLine() 需要转码，readLine() 本身是用 ISO-8859-1 解码的，所以要先用 .getBytes("ISO-8859-1") 编回去，然后再用 utf-8 解码
         System.out.println("name === " + name);
 
         int age = randomAccessFile.readInt();
