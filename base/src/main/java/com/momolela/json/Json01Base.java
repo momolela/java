a97e7d7f9b5edc16173e361814d0e753f40c99e8
sunzj <1083910359@qq.com>
2020-10-12 08:54:16 +0800

    20201012

---
+++ base/src/main/java/com/momolela/json/Json01Base.java
@@ -0,0 +1,9 @@
+package com.momolela.json;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class Json01Base {
+    public static void main(String[] args) {
+        System.out.println(JSONObject.parseObject("nihao"));
+    }
+}
