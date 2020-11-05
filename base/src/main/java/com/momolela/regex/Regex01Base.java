cff0d4cd10a8b46116e28af8afe40d78fbb278b1
sunzj <1083910359@qq.com>
2020-11-05 22:06:33 +0800

    20201105

--- base/src/main/java/com/momolela/regex/Regex01Base.java
+++ base/src/main/java/com/momolela/regex/Regex01Base.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 public class Regex01Base {
     public static void main(String[] args) {
+        // 将 ## 替换为 #{}
         String line = "nihao#dfd#ggfgd#fg#gfd";
         Pattern pattern = Pattern.compile("\\#[A-Za-z0-9_]*\\#");
         Matcher matcher = pattern.matcher(line);
@@ -14,5 +15,14 @@ public class Regex01Base {
             line = line.replace(ori, str);
         }
         System.out.println(line);
+
+        // 取出 bsoft_service[] 中括号中的内容
+        String regex = "bsoft_service\\[(.*?)]";
+        Pattern pattern1 = Pattern.compile(regex);
+        Matcher matcher1 = pattern1.matcher("nihao#dfbsoft_service[d#]ggfgd#fbsoft_service[g#gf]d");
+        while (matcher1.find()) {
+            System.out.println(matcher1.group(1));
+            break;
+        }
     }
 }
