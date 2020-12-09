1c4c6da21a79da7c2efb4979056e1b78cf9345cf
sunzj <1083910359@qq.com>
2020-12-09 23:42:03 +0800

    20201209

--- base/src/main/java/com/momolela/system/System01Base.java
+++ base/src/main/java/com/momolela/system/System01Base.java
@@ -1,5 +1,7 @@
 package com.momolela.system;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Properties;
 
 public class System01Base {
@@ -7,28 +9,33 @@ public class System01Base {
     public static void main(String[] args) {
         System.out.println("标准输出\n");
 
+
         System.setProperty("sunzj", "hufy");
+
 
         // 以此来做跨平台处理的操作
         System.getProperty("os.name");
 
-        /**
-         * Properties 是 HashTable 的子类，也就是 Map 接口的子类，所以能用 Map 接口公共的方法
-         */
+
+        // Properties 是 HashTable 的子类，也就是 Map 接口的子类，所以能用 Map 接口公共的方法
         Properties properties = System.getProperties();
         for (Object obj : properties.keySet()) {
             Object value = properties.get(obj);
             System.out.println(obj + " === " + value);
         }
 
-        /**
-         * Properties 是 HashTable 的子类，是与流相结合的集合，下面也可以输出到控制台
-         */
+
+        // Properties 是 HashTable 的子类，是与流相结合的集合，下面也可以输出到控制台
         properties.list(System.out);
 
-        /**
-         * 在 jvm 启动的时候 setProperty，可以用 java -D<key>=<value> 程序字节码
-         */
+
+        // 在 jvm 启动的时候 setProperty，可以用 java -D<key>=<value> 程序字节码
+
+
+        // 打印当前时间的毫秒数
+        System.out.println(System.currentTimeMillis());
+        System.out.println(new Date().getTime());
+        System.out.println(Calendar.getInstance().getTimeInMillis());
     }
 
 }
