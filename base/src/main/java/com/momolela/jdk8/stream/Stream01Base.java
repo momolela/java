52328e3328868ba13060ff3da5da3e857c7a249e
momolela <1083910359@qq.com>
2021-08-23 23:58:03 +0800

    20210823

--- base/src/main/java/com/momolela/jdk8/stream/Stream01Base.java
+++ base/src/main/java/com/momolela/jdk8/stream/Stream01Base.java
@@ -150,6 +150,7 @@ public class Stream01Base {
         System.out.println(max);
         System.out.println("========== 10");
 
+
         // min
         // 取最小值
         // 返回的是 Optional
@@ -157,11 +158,13 @@ public class Stream01Base {
         System.out.println(min);
         System.out.println("========== 11");
 
+
         // count
         // 取个数
         long count = Stream.of(5, 1, 2, 6, 3, 7, 4).count();
         System.out.println(count);
         System.out.println("========== 12");
+
 
         // reduce
         // --- 接受的参数是 Optional<T> reduce(BinaryOperator<T> accumulator);
@@ -173,18 +176,18 @@ public class Stream01Base {
         }).get();
         System.out.println(result131);
         // --- 接受的参数是 T reduce(T identity, BinaryOperator<T> accumulator);
-        // 指定了初始值为 0
+        // 指定了初始值为 0，即使 stream 为空也不会返回 null
         Integer result132 = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).reduce(5, (a, b) -> {
             System.out.println("a2=" + a + ",b2=" + b);
             return a + b;
         });
         System.out.println(result132);
         // --- 接受的参数是 <U> U reduce(U identity, BiFunction<U, ? super T, U> accumulator, BinaryOperator<U> combiner);
-        //
+        // 将 int 类型的列表合并成 long 类型的结果
         List<Integer> numList = Arrays.asList(Integer.MAX_VALUE, Integer.MAX_VALUE);
         long result133 = numList.stream().reduce(0L, (a, b) -> a + b, (a, b) -> 0L);
         System.out.println(result133);
-        // 新应用
+        // 新应用，将一个 int 类型的 ArrayList 转换成一个 String 类型的 ArrayList
         List<Integer> numList134 = Arrays.asList(1, 2, 3, 4, 5, 6);
         ArrayList<String> result134 = numList134.stream().reduce(new ArrayList<String>(), (a, b) -> {
             a.add("element-" + Integer.toString(b));
@@ -192,6 +195,113 @@ public class Stream01Base {
         }, (a, b) -> null);
         System.out.println(result134);
         System.out.println("========== 13");
+
+
+        // flatMap
+        // 将每个元素替换成新的元素，用于改变流中元素本身类型
+        // flatMap 可以将一个二维的集合映射成一个一维的集合
+        Stream<String> stream141 = Stream.of("1 2", "3 4", "5 6");
+        stream141.flatMap(item -> Arrays.stream(item.split(" "))).forEach(System.out::println);
+        Stream<String> stream142 = Stream.of("1 2", "3 4", "5 6");
+        // 可见，用map()方法，返回了一个“流中流”，需要在每个Stream元素遍历时，再加一层forEach进行遍历
+        stream142.map(item -> Arrays.stream(item.split(" "))).forEach(n -> n.forEach(System.out::println));
+        System.out.println("========== 14");
+
+
+        // collect
+        // collect()方法的参数为一个java.util.stream.Collector类型对象，可以用java.util.stream.Collectors工具类提供的静态方法来生成，Collectors类实现很多的归约操作，如Collectors.toList()、Collectors.toSet()、Collectors.joining()（joining适用于字符串流）等
+        // --- Collectors.toList() 转成 List
+        List<Integer> numbers151 = Arrays.asList(-1, -2, 0, 4, 5);
+        List<Integer> absList151 = numbers151.stream().map(n -> Math.abs(n)).collect(Collectors.toList());
+        //List<Integer> absList151 = numbers151.stream().map(n -> Math.abs(n)).collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(absList151);
+        // --- Collectors..joining(",") 合并成 String
+        List<String> strings152 = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
+        String mergedString152 = strings152.stream().filter(string -> !string.isEmpty()).collect(Collectors.joining(", ")); // abc, bc, efg, abcd, jkl
+        //String mergedString152 = strings152.stream().filter(string -> !string.isEmpty()).collect(Collectors.joining("")); // abcbcefgabcdjkl
+        //String mergedString152 = strings152.stream().filter(string -> !string.isEmpty()).collect(Collectors.joining(", ", "{", "}")); // {abc, bc, efg, abcd, jkl}
+        System.out.println(mergedString152);
+        // --- Collectors.groupingBy(key, value) 生成一个 map
+        List<Book> bookList153 = new ArrayList<>();
+        bookList153.add(new Book("sunzj", 50));
+        bookList153.add(new Book("hufy", 20));
+        bookList153.add(new Book("sunzj", 0));
+        bookList153.add(new Book("sunzj", 0));
+        Map<Integer, List<Book>> result153 = bookList153.stream().collect(Collectors.groupingBy(Book::getPrice)); // {0=[Book(name=sunzj, price=0), Book(name=sunzj, price=0)], 50=[Book(name=sunzj, price=50)], 20=[Book(name=hufy, price=20)]}
+        //Map<Integer, Long> result153 = bookList153.stream().collect(Collectors.groupingBy(Book::getPrice, counting())); // {0=2, 50=1, 20=1}
+        System.out.println(result153);
+        // --- Collectors.toMap(key, value) 生成一个 map
+        Map<Integer, String> result154 = bookList153.stream().distinct().collect(Collectors.toMap(Book::getPrice, Book::getName));// {0=sunzj, 50=sunzj, 20=hufy}
+        //Map<Integer, Book> result154 = bookList153.stream().distinct().collect(Collectors.toMap(Book::getPrice, Book -> Book)); // {0=Book(name=sunzj, price=0), 50=Book(name=sunzj, price=50), 20=Book(name=hufy, price=20)}
+        System.out.println(result154);
+        // --- Collectors.toSet() 转成 Set
+        Set<Book> result155 = bookList153.stream().collect(Collectors.toSet());
+        //Set<Book> result155 = bookList153.stream().collect(Collectors.toCollection(HashSet::new));
+        System.out.println(result155);
+        // --- Collectors.maxBy() Collectors.minBy() Collectors.summingInt() Collectors.averagingInt() Collectors.counting()
+        Book maxBook = bookList153.stream().collect(Collectors.maxBy((b1, b2) -> b1.getPrice() - b2.getPrice())).get();
+        System.out.println(maxBook);
+        // --- Collectors.partitioningBy() 分区分组，只能分成 true 和 false 两组
+        Map<Boolean, List<Book>> result156 = bookList153.stream().collect(Collectors.partitioningBy(b -> b.getPrice() > 20));
+        System.out.println(result156);
+        System.out.println("========== 15");
+
+
+        // toArray
+        List<String> stringList161 = Arrays.asList("AA", "BB", "CC", "BB", "BB", "CC");
+        // --- 无参
+        Object[] result161 = stringList161.stream().toArray();
+        for (int i = 0; i < result161.length; i++) {
+            System.out.println(result161[i]);
+        }
+        // --- 参数 IntFunction<A[]> generator
+        String[] result162 = stringList161.stream().toArray(String[]::new);
+        for (int i = 0; i < result162.length; i++) {
+            System.out.println(result162[i]);
+        }
+        System.out.println("========== 16");
+
+
+        // forEachOrdered
+        List<String> stringList171 = Arrays.asList("A", "B", "C", "D");
+        stringList171.stream().parallel().forEach(System.out::println); // 顺序不定
+        stringList171.stream().parallel().forEachOrdered(System.out::println); // A B C D
+        System.out.println("========== 17");
+
+
+        // anyMatch allMatch noneMatch count
+        List<String> stringList181 = Arrays.asList("a", "a", "a", "a", "b");
+        boolean aa = stringList181.stream().anyMatch(str -> str.equals("a"));
+        boolean bb = stringList181.stream().allMatch(str -> str.equals("a"));
+        boolean cc = stringList181.stream().noneMatch(str -> str.equals("a"));
+        long count18 = stringList181.stream().filter(str -> str.equals("a")).count();
+        System.out.println(aa); // true
+        System.out.println(bb); // false
+        System.out.println(cc); // false
+        System.out.println(count18); // 4
+        List<String> stringList182 = new ArrayList<>();
+        boolean allMatch = stringList182.stream().allMatch(e -> e.equals("a"));
+        boolean anyMatch = stringList182.stream().anyMatch(e -> e.equals("a"));
+        boolean noneMatch = stringList182.stream().noneMatch(e -> e.equals("a"));
+        System.out.println(allMatch); // true
+        System.out.println(anyMatch); // false
+        System.out.println(noneMatch); // true
+        System.out.println("========== 18");
+
+
+        // findFirst findAny
+        List<String> stringList191 = Arrays.asList("a", "b", "c", "d", "e");
+        String firstString191 = stringList191.stream().findFirst().get();
+        System.out.println(firstString191);
+        String anyString191 = stringList191.stream().findAny().get();
+        System.out.println(anyString191);
+        System.out.println("========== 19");
+
+
+        // mapToInt mapToDouble mapToLong
+        List<String> stringList201 = Arrays.asList("3", "6", "8", "14", "15");
+        stringList201.stream().mapToInt(s -> Integer.parseInt(s)).filter(num -> num % 3 == 0).forEach(System.out::println);
+        System.out.println("========== 20");
     }
 
     private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
