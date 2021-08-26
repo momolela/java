8b3af9927dd1b8a8fc329c490c9524bc2f667159
momolela <1083910359@qq.com>
2021-08-26 23:42:02 +0800

    20210826

---
+++ java9NewFeatures/java9ModuleTest/src/com/momolela/test/ModuleTest.java
@@ -0,0 +1,12 @@
+package com.momolela.test;
+
+import com.momolela.bean.Person;
+
+public class ModuleTest {
+
+    public static void main(String[] args) {
+        Person person = new Person("sunzj", 26);
+        System.out.println(person.toString());
+    }
+
+}
