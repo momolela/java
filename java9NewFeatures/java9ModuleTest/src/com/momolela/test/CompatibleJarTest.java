1c7d00bb361829decf4850a28c3a3fb444bbeea4
momolela <1083910359@qq.com>
2021-08-27 00:31:10 +0800

    20210827

---
+++ java9NewFeatures/java9ModuleTest/src/com/momolela/test/CompatibleJarTest.java
@@ -0,0 +1,9 @@
+package com.momolela.test;
+
+import com.momolela.Test;
+
+public class CompatibleJarTest {
+    public static void main(String[] args) {
+        Test.sayHello();
+    }
+}
--- java9NewFeatures/java9ModuleTest/src/module-info.java
+++ java9NewFeatures/java9ModuleTest/src/module-info.java
@@ -1,4 +1,5 @@
 module java9ModuleTest{
     requires java9Module;
     requires java.logging;
+    requires test;
 }
