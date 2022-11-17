cc4b42b86d0a663f9727bcb7a9d3a32b17d30af7
sunzj <1083910359@qq.com>
2022-11-17 19:24:30 +0800

    20221117

--- extend/src/main/java/com/momolela/webservice/publish/HelloService.java
+++ extend/src/main/java/com/momolela/webservice/publish/HelloService.java
@@ -1,9 +1,7 @@
 package com.momolela.webservice.publish;
 
-import javax.xml.soap.SOAPException;
-import java.io.IOException;
-
 public interface HelloService {
     String sayHello(String name);
-    String helloBigData() throws IOException, SOAPException;
+
+    String helloBigData() throws Exception;
 }
