159a5514fd6d70cb10a469193cb6fcf1fe14d6f0
sunzj <1083910359@qq.com>
2023-01-03 09:29:14 +0800

    20230103

--- extend/src/main/java/com/momolela/uniqueid/uuid/App.java
+++ extend/src/main/java/com/momolela/uniqueid/uuid/App.java
@@ -1,5 +1,6 @@
 package com.momolela.uniqueid.uuid;
 
+import java.util.Random;
 import java.util.UUID;
 
 public class App {
@@ -7,5 +8,10 @@ public class App {
         for (int i = 0; i < 16; i++) {
             System.out.println(UUID.randomUUID().toString().replace("-", ""));
         }
+
+        // 随机 400 到 500 的整数
+        Random random = new Random();
+        int s = random.nextInt(500) % (500 - 400 + 1) + 400;
+        System.out.println(s);
     }
 }
