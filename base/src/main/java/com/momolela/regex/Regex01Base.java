d6438d7d296fb87fdb2fd01b12999eab09b87852
sunzj <1083910359@qq.com>
2023-02-25 17:26:27 +0800

    20230225

--- base/src/main/java/com/momolela/regex/Regex01Base.java
+++ base/src/main/java/com/momolela/regex/Regex01Base.java
@@ -1,5 +1,6 @@
 package com.momolela.regex;
 
+import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -47,25 +48,47 @@ public class Regex01Base {
         String regex = "bsoft-service: (.*?)\\r\\n";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher("nihao#dfbsoft-service: nihao\r\nggfgd#fbsoft-service: [g#gf]\r\nd");
+
         // appendReplacement()
+        // StringBuffer sb1 = new StringBuffer();
         // while (matcher.find()) {
-        //     StringBuffer sb = new StringBuffer();
         //     // 将当前匹配子串替换为指定字符串，
         //     // 并且将替换后的子串以及其之前到上次匹配子串之后的字符串段添加到一个 StringBuffer 对象里
-        //     matcher.appendReplacement(sb, "sunzj");
-        //     System.out.println(sb);
+        //     matcher.appendReplacement(sb1, "sunzj"); // 分别添加 nihao#dfsunzj 和 ggfgd#fsunzj
         // }
+        // System.out.println(sb1); // nihao#dfsunzjggfgd#fsunzj
 
-        // appendTail()
-        while (matcher.find()) {
-            StringBuffer sb = new StringBuffer();
-            // 方法则将最后一次匹配工作后剩余的字符串添加到一个 StringBuffer 对象里
-            matcher.appendTail(sb);
-            System.out.println(sb);
-        }
-        // replaceAll()
-        // replaceFirst()
+        // appendTail() 替换所有
+        // StringBuffer sb2 = new StringBuffer();
+        // while (matcher.find()) {
+        //     // 将最后一次匹配工作后剩余的字符串添加到一个 StringBuffer 对象里
+        //     matcher.appendReplacement(sb2, "sunzj");
+        // }
+        // matcher.appendTail(sb2);
+        // System.out.println(sb2); // nihao#dfsunzjggfgd#fsunzjd
+
+        // appendTail() 替换第一个
+        // StringBuffer sb3 = new StringBuffer();
+        // if (matcher.find()) {
+        //     // 将最后一次匹配工作后剩余的字符串添加到一个 StringBuffer 对象里
+        //     matcher.appendReplacement(sb3, "sunzj");
+        // }
+        // matcher.appendTail(sb3);
+        // System.out.println(sb3); // nihao#dfsunzjggfgd#fbsoft-service: [g#gf]\r\nd
+
+        // replaceAll() 替换所有
+        // String s4 = matcher.replaceAll("sunzj");
+        // System.out.println(s4);
+
+        // replaceFirst() 替换第一个
+        // String s5 = matcher.replaceFirst("sunzj");
+        // System.out.println(s5);
+
         // quoteReplacement()
+        // 得知，在使用replaceAll时，如果替换的字符中包含''或者'Text parse error !'符号可能会导致意想不到的结果。因为替换时使用了正则表达式，而'\'和''是正则中的关键字，替换会造成混淆。
+        // 解决办法：使用 Matcher.quoteReplacement(sep) 转义。
+        String s6 = "sunzj/r/nhaha".replaceAll(Matcher.quoteReplacement(File.separator), ".");
+        System.out.println(s6);
     }
 
     /**
