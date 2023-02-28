212c42d022cd9a7faa3c5ae18f044b90d8f4a61d
sunzj <1083910359@qq.com>
2023-02-28 08:51:33 +0800

    20230228

---
+++ designPattern/src/main/java/com/momolela/templatepattern/TemplatePatternDemo.java
@@ -0,0 +1,14 @@
+package com.momolela.templatepattern;
+
+public class TemplatePatternDemo {
+    public static void main(String[] args) {
+
+        Game game = new Cricket();
+        game.play();
+
+        System.out.println();
+
+        game = new Football();
+        game.play();
+    }
+}
