e94b1dc64f32f91e4e1adb45e48a5ce184a8152b
sunzj <sunzj@bsoft.com.cn>
2023-08-25 08:53:28 +0800

    20230825

--- base/src/main/java/com/momolela/net/http/proxy/HttpProxy99InvokeDemo.java
+++ base/src/main/java/com/momolela/net/http/proxy/HttpProxy99InvokeDemo.java
@@ -136,12 +136,12 @@ public class HttpProxy99InvokeDemo {
     public static void testHaiHttp() {
         try {
             DefaultHttpClient httpclient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost("http://10.8.2.161:9526/hai/HttpEntry/"); // url 请求地址，最后要加上 /，不然 post 请求失败
+            HttpPost httpPost = new HttpPost("http://10.8.2.167:9526/hai/HttpEntry/"); // url 请求地址，最后要加上 /，不然 post 请求失败
             List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-            nvps.add(new BasicNameValuePair("service", "ODS_listQueues")); // service 流程名称（必填）
+            nvps.add(new BasicNameValuePair("service", "getFTeamList")); // service 流程名称（必填）
             nvps.add(new BasicNameValuePair("urid", "")); //urid 用户名
             nvps.add(new BasicNameValuePair("pwd", "")); //pwd 密码
-            nvps.add(new BasicNameValuePair("parameter", "<BSXml><MsgHeader><Organization>1</Organization><Sender>GOL</Sender><ServiceType>service</ServiceType><MsgType>ODS_02080001</MsgType><MsgVersion>3.3</MsgVersion></MsgHeader><MsgBody> <VisitOrganization>1</VisitOrganization> <BusinessType>1</BusinessType> <QueuesId>44</QueuesId> </MsgBody></BSXml>")); //parameter 字符串格式
+            nvps.add(new BasicNameValuePair("parameter", "{\"pageNo\":1,\"query\":{\"memberObjId\":\"9600\"},\"pageSize\":-1}")); //parameter 字符串格式
             httpPost.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
             HttpResponse response = httpclient.execute(httpPost);
             HttpEntity entity = response.getEntity();
